7c9ef41ecd5415ffb9c3348a4b83a938e899a8d4
siriphong <siriphong@heartworks.io>
2019-07-24 11:40:23 +0700

    integrate subtaskModel to todoModel

--- models/TodoModel.js
+++ models/TodoModel.js
@@ -1,9 +1,12 @@
 const bookshelf = require('../db')
-
+const SubtaskModel = require("./SubtaskModel");
 
 const TodoModel = bookshelf.Model.extend({
   tableName: 'todos',
   hasTimestamps: true,
+  subTask() {
+    return this.hasMany(SubtaskModel);
+  }
 })
 
 module.exports = TodoModel;
